4d5be59f8b98e236df896a1c8991d5ae82d72ee5
Surender Gupta <surender@auxinov.ventures>
2026-02-04 02:51:29 +0530

    Implement feature section

--- src/store/index.js
+++ src/store/index.js
@@ -3,15 +3,18 @@ import { create } from "zustand";
 const useMacbookStore = create((set) => ({
   color: "#2e2c2e",
   setColor: (color) => set({ color }),
-  clearColor: () => set({ color: null }),
 
   scale: 0.08,
   setScale: (scale) => set({ scale }),
+
+  texture: "/videos/feature-1.mp4",
+  setTexture: (texture) => set({ texture }),
 
   reset: () =>
     set({
       color: "#2e2c2e",
       scale: 0.08,
+      texture: "/videos/feature-1.mp4",
     }),
 }));
 
